ee7ffe44879d2327b422cf57eb89241e84b686a3
Justin-Cordoba <jcordobasevilla@gmail.com>
2025-04-09 13:02:57 -0600

    change name

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'FELIZ', 'CUMPLE','JUSTIN'],
+			strings: [ 'FELIZ', 'CUMPLE','JORGE 🫡'],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
